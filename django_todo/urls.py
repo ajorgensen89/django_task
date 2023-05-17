802bc40f0ea908fd13bb7f6440920e7d0632c5b4
ajorgensen89 <ajorgensen89@gmail.com>
2023-05-17 15:22:11 +0000

    delete without error status inplace

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('add', views.add_item, name='add_item'),
     path('edit/<item_id>', views.edit_item, name='edit'),
     path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
