c454aa351436fb74b4651e348f476524178ae21b
ajorgensen89 <ajorgensen89@gmail.com>
2023-05-19 15:36:36 +0000

    test 6

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ajorgensen89-djangotask-ojbcobici2h.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['8000-ajorgensen89-djangotask-ojbcobici2h.ws-eu97.gitpod.io', 'aj89-django-task.herokuapp.com']
 # '8000-ajorgensen89-djangotask-ojbcobici2h.ws-eu97.gitpod.io'
 
 
